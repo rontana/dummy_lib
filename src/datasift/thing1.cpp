66a9709611e889234dbeeec111c0704b019035f3
rontana <ron.haskins@datasift.com>
2015-09-02 12:25:11 +0100

    files and things

---
+++ src/datasift/thing1.cpp
@@ -0,0 +1,11 @@
+//
+// Created by ben on 25/08/15.
+//
+
+#include <iostream>
+#include "datasift/thing1.hpp"
+
+void thing1::foo()
+{
+    std::cout << "foo() hello world! then" << std::endl;
+}
---
+++ src/datasift/thing1.hpp
@@ -0,0 +1,15 @@
+//
+// Created by ben on 25/08/15.
+//
+
+#ifndef DUMMY_LIB_THING1_H
+#define DUMMY_LIB_THING1_H
+
+
+class thing1 {
+public:
+    void foo();
+};
+
+
+#endif //DUMMY_LIB_THING1_H
